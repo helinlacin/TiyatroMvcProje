c612fbfbd6cb2b382e3016d3c393acb7cd239386
helinlacin <114211908+helinlacin@users.noreply.github.com>
2023-01-19 23:30:10 +0300

    models guncellendi oyunsalon wiewsda guncelleme yapıldı

---
+++ Models/SeansOyunveSalonModel.cs
@@ -0,0 +1,12 @@
+﻿using EntityLayer;
+
+namespace TiyatroProje.Models
+{
+    public class SeansOyunveSalonModel
+    {
+        public OyunSalon SeansModal { get; set; }
+        public IEnumerable<Oyun> OyunModal { get; set; }
+        public IEnumerable<Salon> SalonModal { get; set; }
+
+    }
+}
