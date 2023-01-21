3e687013e615f450d23040527a3efe8b5e67e7ac
helinlacin <114211908+helinlacin@users.noreply.github.com>
2023-01-21 21:30:50 +0300

    mvc katmanına ve dataccess kısmına paket yuklendi admin controllerda guncelleme yapıldı.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -5,20 +5,22 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using System.Text;
+using XSystem.Security.Cryptography;
 
 namespace TiyatroProje.Controllers
 {
     public class AdminController : Controller
     {
 
-        //private readonly ILogger<AdminController> _logger;
-        //private readonly IToastNotification _toastNotification;
-        //public AdminController(IToastNotification toastNotification, ILogger<AdminController> logger)
-        //{
-        //    _logger = logger;
-        //    _toastNotification = toastNotification;
-        //}
+        private readonly ILogger<AdminController> _logger;
+        private readonly IToastNotification _toastNotification;
 
+        public AdminController(ILogger<AdminController> logger, IToastNotification toastNotification)
+        {
+            _logger = logger;
+            _toastNotification = toastNotification;
+        }
 
         [HttpGet]
         public IActionResult login()
@@ -30,6 +32,7 @@ namespace TiyatroProje.Controllers
         {
             return View();
         }
+        
 
     }
 }
